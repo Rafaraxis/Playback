cf20784994a16d48fd8d418682c7753d5d63e4e5
Rafaraxis <rafaelvacr@gmail.com>
2018-01-24 09:51:53 -0700

    Repositioning

--- Playback/MainWindow.xaml.cs
+++ Playback/MainWindow.xaml.cs
@@ -27,19 +27,22 @@ namespace Playback
         private Mp3FileReader reader;
         private WaveOut output;
         DispatcherTimer timer;
+        bool dragging = false;
 
         public MainWindow()
         {
             InitializeComponent();
             timer = new DispatcherTimer();
-            timer.Interval = TimeSpan.FromMilliseconds(100);
+            timer.Interval = TimeSpan.FromMilliseconds(1000);
             timer.Tick += OnTimerTick;
 
-            sldPosition.DragLeave += sldPosition_DragCompleted;
+            sldPosition.DragEnter
+
+            += sldPosition_DragCompleted;
         }
         private void OnTimerTick(object sender, EventArgs  e)
         {
-            if (reader != null)
+            if (reader != null && !dragging)
             {
                 lblPosition.Text = reader.CurrentTime.ToString();
                 sldPosition.Value = reader.CurrentTime.TotalSeconds;
@@ -92,12 +95,20 @@ private void btnStop_Click(object sender, RoutedEventArgs e)
                 btnStop.IsEnabled = false;
             }
         }
+        private void sldPosirion_DragStarted(object sender, RoutedEventArgs e)
+        {
+            if (reader != null)
+            {
+                dragging = true;
+            }
 
+        }
         private void sldPosition_DragCompleted(object sender, RoutedEventArgs e)
         {
             if (reader != null)
             {
                 reader.CurrentTime = TimeSpan.FromSeconds(sldPosition.Value);
+                dragging = false;
             }
         }
     }
